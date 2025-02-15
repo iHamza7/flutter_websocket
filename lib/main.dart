2bd8119d2a60c68f39411927932918301c7c5306
iHamza7 <khanazam3324@gmail.com>
2025-02-15 21:58:47 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_websocket/feature/home/screens/home_screen.dart';
 
 void main() {
   runApp(ProviderScope(child: const MyApp()));
@@ -11,11 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'WebSockets channel',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: HomeScreen(),
     );
   }
 }
