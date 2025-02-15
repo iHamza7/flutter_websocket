7cf99fff89a57497ba63da83d41fa5b663624f25
iHamza7 <khanazam3324@gmail.com>
2025-02-15 22:09:15 +0500

    changed

---
+++ lib/feature/api/api.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+final coinbaseWebScoketProvider = Provider<CoinbaseWebSocket>((ref) {
+  ref.onDispose(() {
+    debugPrint("socket disposed");
+  });
+  ref.onCancel(() {
+    debugPrint("socket cancel");
+  });
+  ref.onAddListener(() {
+    debugPrint("socket added");
+  });
+  ref.onResume(() {
+    debugPrint("socket resume");
+  });
+  ref.onRemoveListener(() {
+    debugPrint("socket removed");
+  });
+  return CoinbaseWebSocket();
+});
+
+class CoinbaseWebSocket {
+  final url = "wss://ws-feed-public.sandbox.exchange.coinbase.com";
+
+  WebSocketChannel connect() {
+    return WebSocketChannel.connect(Uri.parse(url));
+  }
+}
