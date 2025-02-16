ef9356de7b64747fabe063a2cc87ec98fa465043
iHamza7 <khanazam3324@gmail.com>
2025-02-16 21:31:00 +0500

    change

--- lib/feature/api/api.dart
+++ lib/feature/api/api.dart
@@ -4,7 +4,7 @@ import 'package:web_socket_channel/web_socket_channel.dart';
 
 final coinbaseWebScoketProvider = Provider<CoinbaseWebSocket>((ref) {
   ref.onDispose(() {
-    debugPrint("socket disposed nvhjiklm");
+    debugPrint("socket disposed nvhjiklmh");
   });
   ref.onCancel(() {
     debugPrint("socket cancel");
