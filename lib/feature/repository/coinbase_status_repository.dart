41987ac11d403120d181df127312f64028a62aeb
iHamza7 <khanazam3324@gmail.com>
2025-02-16 21:25:32 +0500

    changed

---
+++ lib/feature/repository/coinbase_status_repository.dart
@@ -0,0 +1,53 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_websocket/feature/api/api.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+final coinbaseStatusRepositoryProvider =
+    AutoDisposeStreamProvider<Map<String, dynamic>>((ref) async* {
+  final coinbaseSocket = ref.watch(coinbaseWebScoketProvider);
+});
+
+class CoinbaseStatusRepository {
+  final CoinbaseWebSocket _coinbaseWebSocket;
+
+  CoinbaseStatusRepository(this._coinbaseWebSocket) {
+    _init();
+  }
+
+  WebSocketChannel? _channel;
+  final bool _isDispose = false;
+  bool _isSubscribed = false;
+  final StreamController<Map<String, dynamic>> _streamController =
+      StreamController<Map<String, dynamic>>();
+  Stream<Map<String, dynamic>> get stream => _streamController.stream;
+
+  void _init() {
+    _channel = _coinbaseWebSocket.connect();
+    _subscribeToChannel();
+    _listen();
+  }
+
+  void _subscribeToChannel() {
+    if (_isDispose) return;
+    final message = jsonEncode({
+      "type": "subscribe",
+      "channels": [
+        {"name": "status"}
+      ]
+    });
+
+    _isSubscribed = true;
+    _channel?.sink.add(message);
+  }
+
+  void _listen() {
+    if (_isDispose) return;
+    _channel?.stream.listen((data) {
+      final jsonData = jsonDecode(data) as Map<String, dynamic>;
+      _streamController.add(jsonData);
+    }, onDone: () {}, cancelOnError: true);
+  }
+}
