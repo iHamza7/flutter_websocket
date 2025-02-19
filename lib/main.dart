ce1360906c62c05f19990453defda4a43f1574d0
iHamza7 <khanazam3324@gmail.com>
2025-02-19 18:58:40 +0500

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'WebSockets cha',
+      title: 'WebSockets chann',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
