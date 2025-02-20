07eb78f1eadb740b1045b8cef3d7c9b3cc2df96e
iHamza7 <khanazam3324@gmail.com>
2025-02-20 21:18:35 +0500

    changed

--- lib/feature/home/widgets/coin_status_list.dart
+++ lib/feature/home/widgets/coin_status_list.dart
@@ -17,7 +17,7 @@ class CoinStatusList extends ConsumerWidget {
               final coinData = coins[index];
               return ListTile(
                 title: Text(coinData["id"]),
-                subtitle: Text(coinData["status j"]),
+                subtitle: Text(coinData["status jhajfd"]),
               );
             },
             itemCount: coins.length,
