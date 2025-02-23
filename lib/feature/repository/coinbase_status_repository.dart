23155465824d46c720e2ee666702f341ae2e4b72
iHamza7 <khanazam3324@gmail.com>
2025-02-23 21:44:01 +0500

    changed

--- lib/feature/repository/coinbase_status_repository.dart
+++ lib/feature/repository/coinbase_status_repository.dart
@@ -35,7 +35,7 @@ class CoinbaseStatusRepository {
   void _subscribeToChannel() {
     if (_isDispose) return;
     final message = jsonEncode({
-      "type": "subscribe tqaas",
+      "type": "subscribe tqaasm",
       "channels": [
         {"name": "status"}
       ]
