e656311ced99774ff5dd56012966db6fcc763b60
iHamza7 <khanazam3324@gmail.com>
2025-03-03 12:15:02 +0500

    change

--- lib/feature/models/coin_price_model.dart
+++ lib/feature/models/coin_price_model.dart
@@ -1,11 +1,19 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_websocket/feature/repository/coin_price_repository.dart';
 
+final coinPricemodelProvider =
+    AutoDisposeNotifierProviderFamily<CoinPriceModel, AsyncValue, List<String>>(
+        CoinPriceModel.new);
+
 class CoinPriceModel
     extends AutoDisposeFamilyNotifier<AsyncValue, List<String>> {
   @override
   AsyncValue build(List<String> arg) {
     final priceStream = ref.watch(coinbasePriceRepositoryProvider(arg));
     return priceStream;
+  }
+
+  void reset() {
+    ref.invalidate(coinbasePriceRepositoryProvider);
   }
 }
