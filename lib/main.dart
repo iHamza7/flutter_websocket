c172c569249cb14b7835861b134b45a14bb5b75e
iHamza7 <khanazam3324@gmail.com>
2025-03-09 23:28:08 +0500

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends ConsumerWidget {
     final router = ref.watch(goRouterProvider);
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      title: 'WebSockets',
+      title: 'WebSockets ch',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
