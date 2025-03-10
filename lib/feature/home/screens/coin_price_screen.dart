232cec8fa9dac03122e53e54790b3cb293170afd
iHamza7 <khanazam3324@gmail.com>
2025-03-10 22:53:08 +0500

    changed

--- lib/feature/home/screens/coin_price_screen.dart
+++ lib/feature/home/screens/coin_price_screen.dart
@@ -10,7 +10,7 @@ class CoinPriceScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Coin Price"),
+        title: Text("Coin Price h"),
         actions: [
           IconButton(
               onPressed: () {
